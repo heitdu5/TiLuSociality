0b554b2d0b97b874b1497bd10ac06af62ea37cfd
黑tDo5 <2236702997@qq.com>
2024-08-22 04:00:01 +0000

    删除文件 club-interview/club-interview-api/src/main/java/com/club/interview/api/common/KeyConstants.java

--- club-interview/club-interview-api/src/main/java/com/club/interview/api/common/KeyConstants.java
+++
@@ -1,9 +0,0 @@
-package com.club.interview.api.common;
-
-/**
- * @author Tellsea
- * @date 2024−08−22
- */
-public class KeyConstants {
-    public static final String ALI_ACCESS_KEY = "sk-136d93c7961d4a978624a61f49a12f72";
-}
